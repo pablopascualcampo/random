60e3ee57ada4a1433f98b68b6e474136c71ca1fb
pablopascualcampo <pablo.pascual.campo@alumnos.upm.es>
2015-04-15 22:40:46 +0200

    versionrandom

---
+++ random.js
@@ -0,0 +1,10 @@
+// Math.ramdom() devuelve número aleatorio entre 0 y 1.
+var numero = Math.random();
+
+var str = "Mayor que 0.5";
+
+if(numero <= 0.5){
+	str = "Menor que 0.5";
+}
+
+console.log('\n' + numero + str + '\n');
